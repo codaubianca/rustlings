28dd5d6e19166dbe62ca3f59fca75e9da3873cd9
codaubianca <codau_bianca@yahoo.com>
2024-01-24 13:32:57 +0000

    Finished part 1 (fct, ifs)

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,12 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    if quantity > 40 {
+        quantity
+    } else {
+        quantity * 2
+    }
+}
 
 // Don't modify this function!
+
 #[test]
 fn verify_test() {
     let price1 = calculate_price_of_apples(35);
